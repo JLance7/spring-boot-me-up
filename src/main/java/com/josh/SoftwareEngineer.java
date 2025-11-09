9fb2d153705cc6e6b1b17ed44ecaefa2c2ed9646
Josh L <joshua.lanctot10@gmail.com>
2025-11-09 15:57:07 -0600

    postgres

---
+++ src/main/java/com/josh/SoftwareEngineer.java
@@ -0,0 +1,55 @@
+package com.josh;
+
+import java.util.Objects;
+
+public class SoftwareEngineer {
+    private Integer id;
+    private String name;
+    private String techstack;
+
+    public SoftwareEngineer(){
+
+    }
+
+    public SoftwareEngineer(Integer id, String name, String techstack) {
+        this.id = id;
+        this.name = name;
+        this.techstack = techstack;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTechstack() {
+        return techstack;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTechstack(String techstack) {
+        this.techstack = techstack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(techstack, that.techstack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techstack);
+    }
+}
